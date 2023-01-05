04cfda1499ccdeb51fc9cd0d48962c167259bb72
DariaDzhyhun <dzhigun21@gmail.com>
2023-01-06 01:33:33 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,17 @@
-const counterValue = document.querySelector("#value");
+
+const decrementBtn = document.querySelector ('#decrement[data-action]');
+const incrementBtn = document.querySelector ('#increment[data-action]');
+const counterValue = document.querySelector('#value');
+
+// decrementBtn.addEventListener('click', onDecrementClick);
+// function onDecrementClick() {
+//     counterValue.textContent -= 1;
+    
+// }
+// incrementBtn.addEventListener('click', onIncrementClick);
+// function onIncrementClick() {
+//     counterValue.textContent += 1;
+// }
+
 console.log(counterValue);
+
